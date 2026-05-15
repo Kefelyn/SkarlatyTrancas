1ff12337bbb62118897642675f4d5b245145ef1a
Kefelyn <kefelyn.vitoria4@gmail.com>
2026-05-15 04:18:22 -0300

    adicionando imagens, aplicando responsividade e finalizando o projeto.

--- assets/script.js
+++ assets/script.js
@@ -2,26 +2,13 @@ const carrosel = document.querySelector('.carrosel');
   const imagens = document.querySelectorAll('.carrosel img');
   const btnAnterior = document.querySelector('.botao-anterior');
   const btnPosterior = document.querySelector('.botao-posterior');
-  const bolinhasCarrosel = document.querySelector('.bolinhas-carrosel');
   let index = 0;
 
-  imagens.forEach((_, i) => {
-    const pontinho = document.createElement('div');
-    pontinho.classList.add('bolinhas');
-    if (i === 0) pontinho.classList.add('ativo');
-    pontinho.addEventListener('click', () => {
-      index = i;
-      atualizarCarrosel();
-    });
-    bolinhasCarrosel.appendChild(pontinho);
-  });
 
   
   function atualizarCarrosel(){
-    const bolinhas = document.querySelectorAll('.bolinhas');
     carrosel.style.transform = `translateX(${-index * 100}%)`;
-    bolinhas.forEach(ind => ind.classList.remove('ativo'));
-    bolinhas[index].classList.add('ativo');
+   
   }
 
   btnAnterior.addEventListener('click', () => {
